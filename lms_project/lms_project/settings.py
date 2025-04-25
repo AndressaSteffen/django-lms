4517059899cbf17cb0fb76b18c76ddf3c2528d2e
Andressa Antunes Steffen <andressasteffen1@gmail.com>
2025-04-24 21:18:42 -0300

    Corrige caminho do template de login e finaliza autenticação básica

--- lms_project/lms_project/settings.py
+++ lms_project/lms_project/settings.py
@@ -113,3 +113,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'usuarios.User'
+LOGIN_REDIRECT_URL = '/usuarios/redirecionar/'
--- lms_project/usuarios/views.py
+++ lms_project/usuarios/views.py
@@ -1,5 +1,8 @@
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .forms import CustomUserCreationForm
 from .models import User
 
@@ -8,3 +11,12 @@ class RegistroView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'usuarios/registro.html'
     success_url = reverse_lazy('login')
+
+@login_required
+def redirecionar_usuario(request):
+    if request.user.tipo == 'aluno':
+        return redirect('/cursos/')
+    elif request.user.tipo == 'professor':
+        return redirect('/painel/')
+    else:
+        return redirect('/admin/')
